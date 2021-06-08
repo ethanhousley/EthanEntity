1252d7009636ede2b6fba1e15d43c919186d5f89
ethanhousley <ethan.a.housley@gmail.com>
2021-06-08 11:36:27 -0500

    Initialized Branch

---
+++ EthanEntity/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace EthanEntity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserIdBx = new System.Windows.Forms.TextBox();
+            this.txtCityBx = new System.Windows.Forms.TextBox();
+            this.txtStateBx = new System.Windows.Forms.TextBox();
+            this.txtZipBx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUserIdBx
+            // 
+            this.txtUserIdBx.Location = new System.Drawing.Point(56, 71);
+            this.txtUserIdBx.Name = "txtUserIdBx";
+            this.txtUserIdBx.Size = new System.Drawing.Size(129, 20);
+            this.txtUserIdBx.TabIndex = 0;
+            // 
+            // txtCityBx
+            // 
+            this.txtCityBx.Location = new System.Drawing.Point(56, 114);
+            this.txtCityBx.Name = "txtCityBx";
+            this.txtCityBx.Size = new System.Drawing.Size(129, 20);
+            this.txtCityBx.TabIndex = 1;
+            // 
+            // txtStateBx
+            // 
+            this.txtStateBx.Location = new System.Drawing.Point(56, 159);
+            this.txtStateBx.Name = "txtStateBx";
+            this.txtStateBx.Size = new System.Drawing.Size(129, 20);
+            this.txtStateBx.TabIndex = 2;
+            // 
+            // txtZipBx
+            // 
+            this.txtZipBx.Location = new System.Drawing.Point(56, 201);
+            this.txtZipBx.Name = "txtZipBx";
+            this.txtZipBx.Size = new System.Drawing.Size(129, 20);
+            this.txtZipBx.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "City";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "State";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Zip Code";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(307, 111);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(307, 156);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(307, 198);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(56, 282);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(326, 93);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(307, 68);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 384);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtZipBx);
+            this.Controls.Add(this.txtStateBx);
+            this.Controls.Add(this.txtCityBx);
+            this.Controls.Add(this.txtUserIdBx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserIdBx;
+        private System.Windows.Forms.TextBox txtCityBx;
+        private System.Windows.Forms.TextBox txtStateBx;
+        private System.Windows.Forms.TextBox txtZipBx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ EthanEntity/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace EthanEntity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)                    //SEARCH
+        {
+            using (FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+                
+
+                int U = int.Parse(txtUserIdBx.Text);
+                var searchUser = _dbContext.PrimaryUsers.First(x => x.UserID == U);
+                txtUserIdBx.Text = searchUser.UserID.ToString();
+                txtCityBx.Text = searchUser.City;
+                txtStateBx.Text = searchUser.State;
+                txtZipBx.Text = searchUser.Zip.ToString();
+                
+                MessageBox.Show("User Found.");
+            
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)                  //SAVE
+        {
+            using (FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+
+                if (ValidateField())                    //Validation
+                {
+
+                    var newUser = new PrimaryUser();
+                    newUser.UserID = Int32.Parse(txtUserIdBx.Text);
+                    newUser.State = txtStateBx.Text;
+                    newUser.City = txtCityBx.Text;
+                    newUser.Zip = Int32.Parse(txtZipBx.Text);
+
+
+                    _dbContext.PrimaryUsers.Add(newUser);
+                    _dbContext.SaveChanges();
+                    MessageBox.Show("Changes Saved");
+                }
+
+                else
+                {
+                    MessageBox.Show("Please enter a UserID value.");
+                }
+            }        
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)                //UPDATE
+        {
+
+            using (FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+
+                var upUser = new PrimaryUser();
+                upUser.UserID = Int32.Parse(txtUserIdBx.Text);
+                upUser.State = txtStateBx.Text;
+                upUser.City = txtCityBx.Text;
+                upUser.Zip = Int32.Parse(txtZipBx.Text);
+                
+               
+                _dbContext.Entry(upUser).State = System.Data.Entity.EntityState.Modified;
+                _dbContext.SaveChanges();
+                MessageBox.Show("User Updated.");
+            
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)                    //DELETE
+        {
+            using (FirstDatabaseEntities _dbContext = new FirstDatabaseEntities())
+            {
+
+                int U = int.Parse(txtUserIdBx.Text);
+                var delUser = _dbContext.PrimaryUsers.First(x => x.UserID == U);
+
+
+                _dbContext.PrimaryUsers.Remove(delUser);
+                _dbContext.SaveChanges();
+                MessageBox.Show("User Successfully Deleted.");
+          
+            }
+        }
+
+        public bool ValidateField()                 //ValidationMethod
+        {
+                bool checkVal;
+            
+            if (string.IsNullOrEmpty(txtUserIdBx.Text))
+            {
+                
+                checkVal = false;
+                return checkVal;
+            }
+
+                return checkVal = true;
+
+        }
+    }
+
+
+}
